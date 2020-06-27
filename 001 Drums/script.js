cd70a35d3e5c558ebae048669143defeb272e13d
Patrick Grady <patickrgrady81@gmail.com>
2020-06-27 13:06:47 -0400

    Buttons hilight when button is clicked

--- 001 Drums/script.js
+++ 001 Drums/script.js
@@ -3,44 +3,51 @@ document.addEventListener('keydown', e => {
   let set = false;
 
   e.preventDefault();
-  switch (e.keyCode) {
-    case 65:
-      audio = new Audio('./sounds/boom.wav');
-      set = true;
-      break;
-    case 83:
+  const selected = document.querySelectorAll(`[data-key='${e.keyCode}']`)
+
+  if (selected.length > 0) {
+    selected[0].classList.toggle("active");
+    switch (e.keyCode) {
+      case 65:
+      
+        audio = new Audio('./sounds/boom.wav');
+        set = true;
+        break;
+      case 83:
         audio = new Audio('./sounds/clap.wav');
         set = true;
         break;
-    case 68:
+      case 68:
         audio = new Audio('./sounds/hihat.wav');
         set = true;
         break;
-    case 70:
+      case 70:
         audio = new Audio('./sounds/kick.wav');
         set = true;
         break;
-    case 71:
+      case 71:
         audio = new Audio('./sounds/openhat.wav');
         set = true;
         break;
-    case 72:
+      case 72:
         audio = new Audio('./sounds/ride.wav');
         set = true;
         break;
-    case 74:
+      case 74:
         audio = new Audio('./sounds/snare.wav');
         set = true;
         break;
-    case 75:
+      case 75:
         audio = new Audio('./sounds/tink.wav');
         set = true;
         break;
-    case 76:
+      case 76:
         audio = new Audio('./sounds/tom.wav');
         set = true;
         break;
+    }
+    if (set) audio.play();
+    setTimeout(() => { selected[0].classList.toggle("active") }, 300);
   }
-  if (set) audio.play();
-  
+
  });

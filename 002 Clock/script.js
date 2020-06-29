e237a716b08664763118330ecaae8c9518764d88
Patrick Grady <patickrgrady81@gmail.com>
2020-06-29 12:05:18 -0400

    Added date

--- 002 Clock/script.js
+++ 002 Clock/script.js
@@ -1,22 +1,26 @@
 const getDate = () => { 
   const now = new Date();
   let day = now.getDay();
-  day = dayName(day);
+  const dayName = dayNames(day);
+  const month = now.getMonth() + 1;
+  day = now.getDate();
+  const year = now.getFullYear();
+
   let hour = now.getHours();
   let mins = now.getMinutes();
   let secs = now.getSeconds();
   const suffix = hour > 11 || hour === 0 ? "PM" : "AM";
   hour = hour === 0 ? 12 : hour;
-  hour = hour > 11 ? `${hour - 12}` : `${hour}`;
+  hour = hour > 12 ? `${hour - 12}` : `${hour}`;
   hour = hour < 10 ? `0${hour}` : hour;
   mins = mins < 10 ? `0${mins}` : `${mins}`;
   secs = secs < 10 ? `0${secs}` : `${secs}`;
 
-  return { hour, mins, secs, suffix };
+  return { hour, mins, secs, suffix, dayName, month, day, year };
 
 }
 
-const dayName = (day) => { 
+const dayNames = (day) => { 
   switch (day) { 
     case 0:
       return "Sunday";
@@ -40,6 +44,21 @@ const clearCanvas = (ctx) => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+const drawArm = (change, halfWidth, halfHeight, radius, length, color) => { 
+  const TAU = Math.PI * 2
+
+  const angle = TAU * change;
+  let targetX = halfWidth + Math.cos(angle - (TAU/4)) * (radius * length);
+  let targetY = halfHeight + Math.sin(angle - (TAU/4)) * (radius * length);
+
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(halfWidth, halfHeight);
+  ctx.lineTo(targetX, targetY);
+  ctx.stroke();
+}
+
 const drawClock = ({hour, secs, mins}) => { 
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
@@ -47,7 +66,7 @@ const drawClock = ({hour, secs, mins}) => {
   const halfHeight = canvas.height / 2;
   const offset = 10;
   const radius = halfWidth - offset;
-  const TAU = Math.PI * 2
+
   clearCanvas(ctx);
 
   // Face
@@ -57,50 +76,19 @@ const drawClock = ({hour, secs, mins}) => {
   ctx.arc(halfWidth, halfHeight, radius, 0, 2 * Math.PI);
   ctx.stroke();
 
-  // Hour Hand
-  const hAngle = TAU * (dTime.hour / 12);
-  let hTargetX = halfWidth + Math.cos(hAngle - (TAU/4)) * radius;
-  let hTargetY = halfHeight + Math.sin(hAngle - (TAU/4)) * radius;
-
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = "green";
-  ctx.beginPath();
-  ctx.moveTo(halfWidth, halfHeight);
-  ctx.lineTo(hTargetX, hTargetY);
-  ctx.stroke();
-
-
-
-  // Minute Hand
-  const mAngle = TAU * (dTime.mins / 60);
-  let mTargetX = halfWidth + Math.cos(mAngle - (TAU/4)) * radius;
-  let mTargetY = halfHeight + Math.sin(mAngle - (TAU / 4)) * radius;
-  
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = "blue";
-  
-  ctx.beginPath();
-  ctx.moveTo(halfWidth, halfHeight); 
-  ctx.lineTo(mTargetX, mTargetY); 
-  ctx.stroke();
-
   // Second Hand
-  const sAngle = TAU * (dTime.secs / 60);
-  let sTargetX = halfWidth + Math.cos(sAngle - (TAU/4)) * radius;
-  let sTargetY = halfHeight + Math.sin(sAngle - (TAU / 4)) * radius;
-  
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = "red";
-  
-  ctx.beginPath();
-  ctx.moveTo(halfWidth, halfHeight); 
-  ctx.lineTo(sTargetX, sTargetY); 
-  ctx.stroke();
+  drawArm(dTime.secs / 60, halfWidth, halfHeight, radius, 0.9, "red");
+   // Minute Hand
+  drawArm(dTime.mins / 60, halfWidth, halfHeight, radius, 0.8, "green");
+  // Hour Hand
+  drawArm(dTime.hour / 12, halfWidth, halfHeight, radius, 0.5, "blue");
+
 }
 
 const updateTime = () => { 
   dTime = getDate();
-  document.getElementsByClassName("dTime")[0].innerText = `${dTime.hour}:${dTime.mins}:${dTime.secs} ${dTime.suffix}`;
+  document.getElementsByClassName("dTime")[0].innerText = `${dTime.dayName} ${dTime.month}/${dTime.day}/${dTime.year}`;
+  document.getElementsByClassName("dTime")[1].innerText = `${dTime.hour}:${dTime.mins}:${dTime.secs} ${dTime.suffix}`;
 
   drawClock(dTime);
 }
